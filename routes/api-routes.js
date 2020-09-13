6caf91024e0ed5c470f489b7f56b1d3c667267cb
Thad Cole <thadjcole@gmail.com>
2020-09-13 13:53:04 -0400

    stats page displays data from all workouts in db

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,7 @@ const db = require("../models");
 
 // * export routes associated with the API
 module.exports = (app) => {
-  // get all workouts from db
+  // get all workouts from db for last workout
   app.get("/api/workouts", async (req, res) => {
     try {
       const allWorkouts = await db.Workout.find({});
@@ -13,15 +13,23 @@ module.exports = (app) => {
     }
   });
 
-  app.get("/api/workouts/range", (req, res) => {
-    // TODO get workouts within a specific range from db
+  // get all workouts from db for stats page
+  app.get("/api/workouts/range", async (req, res) => {
+    try {
+      const workoutData = await db.Workout.find({});
+      res.status(200).json(workoutData);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   });
 
   app.post("/api/workouts", (req, res) => {
     // TODO add a new workout to db
+    console.log(req);
   });
 
   app.put("/api/workouts/:id", (req, res) => {
     // TODO edit a speciifc workout in db
+    console.log(req);
   });
 };
